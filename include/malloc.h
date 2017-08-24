7725c060171d1251d6c9e037be8aeaf8e9f0ccd8
Charlelie BOUSSAUD <cboussau@e1r6p13.42.fr>
2017-08-24 16:30:15 +0200

    malloc first push

---
+++ include/malloc.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   malloc.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cboussau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/17 15:46:16 by cboussau          #+#    #+#             */
+/*   Updated: 2017/08/24 16:15:11 by cboussau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MALLOC_H
+# define MALLOC_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/resource.h>
+#include <pthread.h>
+
+#define TINY_ZONE		
+#define SMALL_ZONE		
+
+#endif
