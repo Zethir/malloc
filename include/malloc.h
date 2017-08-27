4871be1c34222873e8e5580d9d4156f7433e6dc7
Charlelie BOUSSAUD <charlelie.boussaud@hotmail.fr>
2017-08-27 19:13:20 +0200

    add large alloc

--- include/malloc.h
+++ include/malloc.h
@@ -6,7 +6,7 @@
 /*   By: cboussau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/17 15:46:16 by cboussau          #+#    #+#             */
-/*   Updated: 2017/08/26 16:29:29 by cboussau         ###   ########.fr       */
+/*   Updated: 2017/08/27 18:12:27 by cboussau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,35 @@
 #include <pthread.h>
 #include <libft.h>
 
-#define TINY_ZONE		
-#define SMALL_ZONE		
 
-typedef enum		e_block_type
-{
-	TINY,
-	SMALL,
-	LARGE
-}					t_block_type;					
+#define TINY_SIZE		(size_t)getpagesize() * 2;
+#define	SMALL_SIZE		(size_t)getpagesize() * 16;
+#define TINY_ZONE		(size_t)(TINY_SIZE + sizeof(t_block)) * 100
+#define SMALL_ZONE		(size_t)(SMALL_SIZE + sizeof(t_block)) * 100
 
-typedef struct		s_mem
+#define PROT			PROT_READ | PROT_WRITE
+#define	MAP				MAP_ANON | MAP_PRIVATE
+
+typedef struct		s_block
 {
-	t_block_type	block_type;
-	struct s_mem	*next;
-}					t_mem;
+	size_t			size;
+	int				free;
+	void			*mem;
+	struct s_block	*next;
+}					t_block;
+
+typedef struct		s_zone
+{
+	struct s_block	*tiny_zone;
+	struct s_block	*small_zone;
+	struct s_block	*large_zone;
+}					t_zone;
+
+t_zone				g_zone;
+
+void				*malloc(size_t size);
+void				free(void *ptr);
+void				*realloc(void *ptr, size_t size);
+t_block				*last_node(t_block **head);
 
 #endif
--- src/malloc.c
+++ src/malloc.c
@@ -6,17 +6,52 @@
 /*   By: cboussau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/23 15:39:37 by cboussau          #+#    #+#             */
-/*   Updated: 2017/08/26 15:15:59 by cboussau         ###   ########.fr       */
+/*   Updated: 2017/08/27 19:11:59 by cboussau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <malloc.h>
 
+void	*allocate_large(size_t size)
+{
+	t_block	*block;
+	
+	if ((block = last_node(&(g_zone.large_zone))))
+	{
+		if ((block->next = (t_block *)mmap(0, size, PROT, MAP, -1, 0))
+				== MAP_FAILED)
+			return (MAP_FAILED);
+		block = block->next;
+	}
+	else
+	{
+		if ((g_zone.large_zone = (t_block *)mmap(0, size, PROT, MAP, -1, 0))
+				== MAP_FAILED)
+			return (MAP_FAILED);
+		block = g_zone.large_zone;
+	}
+	block->size = size;
+	block->free = 0;
+	block->mem = block + 1;
+	return (block->mem);
+}
+
 void	*malloc(size_t size)
 {
-	void	*lol;
+	void	*mem;
 
-	lol = NULL;
-	size = 0;
-	return lol;
+/*	
+ 	if (size <= 0)
+		return (NULL);
+ 	if (size <= TINY_SIZE)
+		mem = allocate_tiny(size);
+	else if (size <= SMALL_SIZE)
+		mem = allocate_small(size);
+	else
+	{*/
+		mem = allocate_large(size + sizeof(t_block));
+//	}
+//	if (mem = MAP_FAILED)
+//		return (NULL);
+	return (mem);
 }
